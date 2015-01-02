fd05ee9e14a53be4fc8329346cdb02c2e1321440
akhilesh arora <akhilesh.arora@yahoo.com>
2015-01-02 17:00:53 +0530

    user create seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([{User id:'2'}, {email: "test1@test.com"}, {encrypted_password: " $2a$10$w56E3Iy7qZEK9l4s1Q8qHeXrPjcjNNOzS2S2fyfL0fmz3AT26cLMi"}, {reset_password_token: NULL}, {reset_password_sent_at: NULL}, {remember_created_at: NULL},{sign_in_count: "1"}, {current_sign_in_at: "2015-01-02 08:22:30"}, {last_sign_in_at: "2015-01-02 08:22:30"}, {current_sign_in_ip: '127.0.0.1'}, {last_sign_in_ip: '127.0.0.1'}, {created_at:'2015-01-02 08:22:30'},{updated_at: ' 2015-01-02 08:22:30'}])
